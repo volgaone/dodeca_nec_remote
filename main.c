d2b35de70cf96bf7198a492b21b61be79cc4ec76
volgaone <bigdimon@hotmail.co.uk>
2025-04-09 07:58:20 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: Dmitry
+ *
+ * Created on April 9, 2025, 7:50 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Dodeca_NEC_remote.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Dodeca_NEC_remote.X.production.hex
